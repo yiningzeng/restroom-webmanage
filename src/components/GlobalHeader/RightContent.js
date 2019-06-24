cb85db46438501c4e0f680311cca1bacab4755cc
baymin <yiningzeng@outlook.com>
2019-06-24 22:40:57 +0800

    feat: 去掉无用个人菜单

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -71,19 +71,19 @@ export default class GlobalHeaderRight extends PureComponent {
     } = this.props;
     const menu = (
       <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
-        <Menu.Item key="userCenter">
-          <Icon type="user" />
-          <FormattedMessage id="menu.account.center" defaultMessage="account center" />
-        </Menu.Item>
-        <Menu.Item key="userinfo">
-          <Icon type="setting" />
-          <FormattedMessage id="menu.account.settings" defaultMessage="account settings" />
-        </Menu.Item>
-        <Menu.Item key="triggerError">
-          <Icon type="close-circle" />
-          <FormattedMessage id="menu.account.trigger" defaultMessage="Trigger Error" />
-        </Menu.Item>
-        <Menu.Divider />
+        {/*<Menu.Item key="userCenter">*/}
+          {/*<Icon type="user" />*/}
+          {/*<FormattedMessage id="menu.account.center" defaultMessage="account center" />*/}
+        {/*</Menu.Item>*/}
+        {/*<Menu.Item key="userinfo">*/}
+          {/*<Icon type="setting" />*/}
+          {/*<FormattedMessage id="menu.account.settings" defaultMessage="account settings" />*/}
+        {/*</Menu.Item>*/}
+        {/*<Menu.Item key="triggerError">*/}
+          {/*<Icon type="close-circle" />*/}
+          {/*<FormattedMessage id="menu.account.trigger" defaultMessage="Trigger Error" />*/}
+        {/*</Menu.Item>*/}
+        {/*<Menu.Divider />*/}
         <Menu.Item key="logout">
           <Icon type="logout" />
           <FormattedMessage id="menu.account.logout" defaultMessage="logout" />
@@ -122,7 +122,7 @@ export default class GlobalHeaderRight extends PureComponent {
         ) : (
           <Spin size="small" style={{ marginLeft: 8, marginRight: 8 }} />
         )}
-        <SelectLang className={styles.action} />
+        {/*<SelectLang className={styles.action} />*/}
       </div>
     );
   }
